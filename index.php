e28e9979627601dc7036cc395bc9b45413b23662
Zafar <zafar.asn@gmail.com>
2019-02-28 22:31:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
-echo "Sample application";
+$curl = curl_init();
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://account.demandware.com/dw/oauth2/access_token",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "grant_type=client_credentials",
+  CURLOPT_HTTPHEADER => array(
+    "Authorization: Basic YWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhOmFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYWFhYQ==",
+    "Content-Type: application/x-www-form-urlencoded"
+  ),
+));
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
 ?>
